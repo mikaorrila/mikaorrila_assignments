405e42b1a0b47098432016c9ff9ca73d35b0e374
mikaorrila <mika22022@student.hamk.fi>
2024-11-15 11:06:50 +0200

    Added djangodb

--- mikaorrila_django/first/settings.py
+++ mikaorrila_django/first/settings.py
@@ -76,9 +76,17 @@ WSGI_APPLICATION = 'first.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'djangodb',
+        'USER': 'user_name',
+        'PASSWORD': 'user_password',
+        'HOST': 'localhost',
+        'PORT': '',
     }
+    #'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #}
 }
 
 
